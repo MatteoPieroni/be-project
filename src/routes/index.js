e8294bf1071ae7bb6631302ab506cf75a5a7a3e8
Matteo Pieroni <matteopieroni6@gmail.com>
2020-12-26 00:25:44 +0000

    Added env variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
+import { envVar } from '../config';
+
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: `Welcome to Express API template. Your ENV var is ${envVar}` }));
 
 export default indexRouter;
